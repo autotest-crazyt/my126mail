84ebd9b5974c23d6ebb415ad58abbb9c8148cc5e
yehao <dd@11>
2014-01-28 11:38:54 +0800

    testddddd

--- FireflyAutomation/src/practiceone/PracticeBase.java
+++ FireflyAutomation/src/practiceone/PracticeBase.java
@@ -6,28 +6,28 @@ import org.testng.annotations.*;
 public class PracticeBase {
     @BeforeTest
     public void beforeTest(){
-    	System.out.println("beforeTestbase");
+    	System.out.println("beforeTestbase1");
     }
     
     @AfterTest
     public void afterTest(){
-    	System.out.println("afterTestbase");
+    	System.out.println("afterTestbase1");
     }
     
     @BeforeSuite
     public void beforeSuite(){
-    	System.out.println("beforeSuitebase");
+    	System.out.println("beforeSuitebase1");
     }
     
     @AfterSuite
     public void afterSuite(){
-    	System.out.println("afterSuitebase");
+    	System.out.println("afterSuitebase1");
     }
     
     
     @Test
     public void runAll(){
-    	System.out.println("Run all test casesbase");
+    	System.out.println("Run all test casesbase1");
     }
     
 }
